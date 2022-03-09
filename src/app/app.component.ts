ba6bceeb68d4c694a78a2e5704729c1b2819362e
manesvenom <manesvenom@gmail.com>
2022-03-09 23:05:29 +0800

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,31 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 }
+
+
+@Component({
+  selector: 'joke-list',
+  template:`
+  <div class="card card-block"
+      *ngFor="let joke of jokes">
+      <h4 class="card-title"></h4>
+      <p class="card-text">{{ joke.setup }}</p>
+  </div>
+  `
+})
+export class JokeListComponent{
+  jokes: Array<any>;
+
+  constructor(){
+    this.jokes = [
+      {
+        setup: "Hello2",
+        punchline: "Hello-Me"
+      },
+      {
+        setup: "Hello",
+        punchline: "Hello-Me"
+      }
+    ];
+  }
+}
